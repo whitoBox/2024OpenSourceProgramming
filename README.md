cf992ad2e4f87d0f965707e0bbf0393728ee3d88
John <kimyohan828@gmail.com>
2024-09-27 11:49:41 +0900

    week04 input/output

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository manages and updates program source files and hands-on materials used in open-source programming classes in 2024.
---
+++ week05/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 10
+	fmt.Printf("value a:%d\n", a)
+	fmt.Println("value a:", a)
+	fmt.Print("value a:", a)
+
+}
