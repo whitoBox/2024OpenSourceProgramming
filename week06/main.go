746281373a0d9224e50afe00cbe03e924b38e7d5
John <kimyohan828@gmail.com>
2024-10-04 11:24:46 +0900

    week06.3 zeo value comparison

--- week06/main.go
+++ week06/main.go
@@ -9,10 +9,15 @@ func main() {
 
 	i := 13
 	var f float64 = 12.9
+	c1 := 'A' //룬 4바이트 사용중
+	c2 := '김' //https://www.unicode.org/charts/
 
 	fmt.Printf("value i:%d valuef:%f\n", i, f)
 	//fmt.Printf("%d * %f = %f\n", i, f, i*f)//invalid operation: i * f (mismatched types int and float64)
 	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))     //conversion
 	fmt.Printf("%d * %f = %f\n", i, f, float64(i)*f) //conversion
-	fmt.Print(reflect.TypeOf(i), reflect.TypeOf(f))
+
+	fmt.Print(c1, c2, "\n")
+	fmt.Print(reflect.TypeOf(i), reflect.TypeOf(f), reflect.TypeOf(c1), reflect.TypeOf(c2))
+
 }
