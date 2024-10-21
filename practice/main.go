bb61ac836c6e079517a796ed00c6ccf59d5072c0
John <kimyohan828@gmail.com>
2024-10-21 12:21:25 +0900

    practice uptade stuff

--- practice/main.go
+++ practice/main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	fmt.Print("score:")
-	score := bufio.NewReader(os.Stdin)
-	grade, err := score.ReadString('\n')
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	grade = strings.TrimSpace(grade)
-	finalGrade, _ := strconv.ParseInt(grade, 10, 32)
-
-	if finalGrade >= 90 {
-		fmt.Println("you pass")
-	} else {
-		fmt.Println("you fail")
-	}
+	a := '2'
+	fmt.Println(a)
 }
