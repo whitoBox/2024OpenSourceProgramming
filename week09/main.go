b961eabfc9bd7a3a29c1ed7024933ca89449a271
John <kimyohan828@gmail.com>
2024-10-25 10:43:47 +0900

    number guessing game v0.1

--- week09/main.go
+++ week09/main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	target := rand.Intn(5) + 5
-	fmt.Printf("%d\n", target)
+	answer := rand.Intn(5) + 5
+	//`fmt.Printf("%d\n", answer)
+
+	fmt.Print("input number:")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i) //< .strip in python
+	guess, _ := strconv.Atoi(i)
+
+	if answer == guess {
+		fmt.Println("정답입니다")
+	}
+	if answer > guess {
+		fmt.Println("값이 작다")
+	}
+	if answer < guess {
+		fmt.Println("값이 크다")
+	}
 }
