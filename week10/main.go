53318e02d5e1cbff1f23dcf7453d6a2da095f9d5
John <kimyohan828@gmail.com>
2024-11-01 11:56:01 +0900

    check prime number v0.7

--- week10/main.go
+++ week10/main.go
@@ -9,6 +9,26 @@ import (
 	"strings"
 )
 
+func isPrime(num int) bool { //매게변수 / 리턴타임
+
+	if num < 2 {
+		return false
+	} else if num == 2 {
+
+		return true
+	} else if num%2 == 0 {
+		return false
+	} else {
+		for j := 3; j*j <= num; j += 2 { //sqrt(float), int랑 비교하기 위해서 int
+			if num%j == 0 {
+				return false
+			}
+
+		}
+	}
+	return true
+}
+
 func main() {
 	fmt.Printf("정수 입력: ")
 	r := bufio.NewReader(os.Stdin)
@@ -20,25 +40,7 @@ func main() {
 	i = strings.TrimSpace(i) //< .strip in python
 	num, _ := strconv.Atoi(i)
 
-	isPrime := true
-
-	if num < 2 {
-		isPrime = false
-	} else if num == 2 {
-		isPrime = true
-	} else if num%2 == 0 {
-		isPrime = false
-	} else {
-		for j := 3; j*j <= num; j += 2 { //sqrt(float), int랑 비교하기 위해서 int
-			if num%j == 0 {
-				isPrime = false
-				break
-			}
-			fmt.Printf("%d ", j)
-		}
-	}
-
-	if isPrime {
+	if isPrime(num) {
 		fmt.Printf("%d는 소수입니다.", num)
 	} else {
 		fmt.Printf("%d는 소수아닙입니다.", num)
