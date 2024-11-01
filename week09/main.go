5567d2f9714bb676fe9f0f39c13a70f794d2d858
John <kimyohan828@gmail.com>
2024-11-01 09:57:55 +0900

    Errors

--- week09/main.go
+++ week09/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
-	result := fmt.Sprintf("%v 나누기 %v는 %0.1v 입니다.\n", 1.0, 3.0, 1.0/3.0)
-	print(result)
+	err := errors.New("height can't be negative")
+	fmt.Print(err.Error())
 }
