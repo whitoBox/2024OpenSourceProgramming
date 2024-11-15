461190114de3dbab5abee1b0130d827dc36c9a85
John <kimyohan828@gmail.com>
2024-11-15 10:52:29 +0900

    week12 array var with loop

--- week12/main.go
+++ week12/main.go
@@ -22,5 +22,11 @@ func main() {
 		time.Unix(2, 0),
 		time.Unix(3, 0), //줄 바꿀때도 콤마가 필요하다.
 	}
-	fmt.Println(dates[0], dates[1], dates[2])
+	//fmt.Println(dates[0], dates[1], dates[2])// 배열 값 출력
+	//fmt.Println(dates) // 배열 출력
+	//fmt.Printf("%#v", dates) // 배열 리터럴 출력
+
+	for i := 0; i < len(dates); i++ {
+		fmt.Println(i, dates[i])
+	}
 }
