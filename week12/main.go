76fb8e56639884c4848b0bdcbe4a8bfaff489b48
John <kimyohan828@gmail.com>
2024-11-15 11:20:45 +0900

    week12 var array loop methods

--- week12/main.go
+++ week12/main.go
@@ -26,7 +26,8 @@ func main() {
 	//fmt.Println(dates) // 배열 출력
 	//fmt.Printf("%#v", dates) // 배열 리터럴 출력
 
-	for i := 0; i < len(dates); i++ {
-		fmt.Println(i, dates[i])
+	//for i := 0; i < len(dates); i++ {
+	for i, v := range dates {
+		fmt.Println(i, v)
 	}
 }
