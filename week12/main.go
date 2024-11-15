c6592f8dbaaf9a13dfc02f41217a42a28ba4c6f9
John <kimyohan828@gmail.com>
2024-11-15 11:42:40 +0900

    week12 using others packages

--- week12/main.go
+++ week12/main.go
@@ -1,33 +1,21 @@
 package main
 
-//go mod init
 import (
 	"fmt"
-	"time"
+
+	"github.com/headfirstgo/keyboard"
+	//go get github.com/headfirstgo/keyboard
 )
 
 func main() {
-	// var dates [3]time.Time
-	// things := [2]int{10, 20}
-	// dates[0] = time.Unix(1447920000, 0)
+	var gpa [3]float64
 
-	// fmt.Printf("%d", things)
-	//var dates [3]time.Time = [3]time.Time{time.Unix(1, 0), time.Unix(2, 0), time.Unix(3, 0)}
-
-	// dates := [3]time.Time{time.Unix(1, 0), time.Unix(2, 0), time.Unix(3, 0)}
-	// fmt.Println(dates[0], dates[1], dates[2])
-
-	dates := [3]time.Time{
-		time.Unix(1, 0),
-		time.Unix(2, 0),
-		time.Unix(3, 0), //줄 바꿀때도 콤마가 필요하다.
+	for i := 0; i < len(gpa); i++ {
+		fmt.Print("input float num:")
+		gpa[i], _ = keyboard.GetFloat()
 	}
-	//fmt.Println(dates[0], dates[1], dates[2])// 배열 값 출력
-	//fmt.Println(dates) // 배열 출력
-	//fmt.Printf("%#v", dates) // 배열 리터럴 출력
-
-	//for i := 0; i < len(dates); i++ { //safe way
-	for i, date := range dates { // python method
-		fmt.Println(i, date)
+	for index, value := range gpa {
+		fmt.Printf("%d: %f\n", index, value)
 	}
+
 }
