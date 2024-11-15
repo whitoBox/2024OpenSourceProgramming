247d1edeb7c3952673e89bf890d7031e8d997a99
John <kimyohan828@gmail.com>
2024-11-15 12:20:39 +0900

    week12 type slice

--- week12/main.go
+++ week12/main.go
@@ -2,19 +2,14 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/headfirstgo/keyboard"
+	"reflect"
 )
 
 func main() {
-	var gpa [3]float64
+	gpa := [5]float64{1.1, 2.2, 3.3, 4.4, 4.5}
+	var gpaSlice []float64 = gpa[1:4]
 
-	for i := 0; i < len(gpa); i++ {
-		fmt.Print("input float num:")
-		gpa[i], _ = keyboard.GetFloat() //go get github.com/headfirstgo/keyboard
-	}
-	for index, value := range gpa {
-		fmt.Printf("%d: %f\n", index, value)
-	}
+	fmt.Println(gpaSlice, reflect.TypeOf(gpaSlice))
+	fmt.Println(gpa, reflect.TypeOf(gpa))
 
 }
